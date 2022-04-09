71b3331126d2e5a3da38bbbb1be65e380f757f48
Afeh <balogunafebu22@gmail.com>
2022-04-09 20:14:07 +0100

    Numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,9 +16,12 @@ int main(void)
 	{
 		putchar (numbrz);
 		numbrz++;
-		putchar(',');
-		putchar(' ');
-	}
+		if (numbrz != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
 	putchar ('\n');
 	return (0);
 }
