c551e991eba5659e75fefd29da1e410152c4b33c
Afeh <balogunafebu22@gmail.com>
2022-04-09 19:49:07 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints
+* Return: 0
+*/
+
+int main(void)
+{
+	int numbrz;
+
+	numbrz = '0';
+	while (numbrz <= '9')
+	{
+		putchar (numbrz);
+		numbrz++;
+		putchar(',');
+	}
+	putchar ('\n');
+	return (0);
+}
