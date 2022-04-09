d45e5d1f671e61525b032dc4808ec4d387ffedff
Afeh <balogunafebu22@gmail.com>
2022-04-09 14:59:10 +0100

    Nuberz with char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints
+* Return: 0
+*/
+
+int main(void)
+{
+	int numbrz;
+
+	for (numbrz = 0; numbrz < 10; numbrz++)
+		putchar (numbrz);
+	putchar ('\n');
+	return (0);
+}
