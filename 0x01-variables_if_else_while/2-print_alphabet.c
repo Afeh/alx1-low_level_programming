6cae1622e6ff2aea1ea59203bb96839be237b206
Afeh <balogunafebu22@gmail.com>
2022-04-09 13:28:09 +0100

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints
+* Return: 0
+*/
+
+int main(void)
+{
+	char alph;
+
+	alph = 'a';
+	while (alph < 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	return (0);
+}
