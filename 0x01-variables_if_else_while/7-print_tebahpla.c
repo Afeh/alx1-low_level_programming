48c0ece4e99c540a39b8aa01dd48a960e9367315
Afeh <balogunafebu22@gmail.com>
2022-04-09 17:06:46 +0100

    Nuberz with char

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints
+* Return: 0
+*/
+
+int main(void)
+{
+	char alph;
+
+	alph = 'z';
+	while (alph >= 'a')
+	{
+		putchar(alph);
+		alph--;
+	}
+	putchar('\n');
+	return (0);
+}
