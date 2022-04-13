47e93be011c4ac580b1825bfb7dee652279e1b6d
Afeh <balogunafebu22@gmail.com>
2022-04-13 22:40:42 +0100

    Push

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
