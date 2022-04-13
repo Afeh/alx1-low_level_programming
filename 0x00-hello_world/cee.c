82aca82dcd646b0d08388835d039e1f178b3e964
Afeh <balogunafebu22@gmail.com>
2022-04-13 22:26:50 +0100

    P

---
+++ 0x00-hello_world/cee.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ */
+
+
+int main(void)
+{
+
+while (a = 0; a < b; a++)
+{
+    printf("%d\n", a);
+}
+
+  return (0);
+}
