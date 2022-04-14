ca4349292a69d52dfa43fe7a0c93d9ec7c3b495c
Afeh <balogunafebu22@gmail.com>
2022-04-14 10:46:32 +0100

    a-z ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char i = 0, c;
+	while (i<10)
+	{
+		c = 'a';
+		while(c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+	_putchar('\n');
+	i++;
+	}
+}
