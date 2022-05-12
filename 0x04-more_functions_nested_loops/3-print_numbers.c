41d23d1804e05ff52343add5bb69dada50404d8d
Afeh <balogunafebu22@gmail.com>
2022-05-12 22:41:34 +0100

    Prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints with the putchar
+ *
+ * Returns: numbers
+ */
+
+void print_numbers(void)
+{
+	int b;
+
+	for (b = 48; b <= 57; b++)
+		_putchar(b);
+	_putchar('\n');
+}
